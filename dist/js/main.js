be3922b0519e5586b9699b6fca20c00b513f585f
kgaolodevs <kgaolodev@gmail.com>
2020-07-27 19:52:26 +0200

    Added burger menu

---
+++ dist/js/main.js
@@ -0,0 +1,6 @@
+// DOM Selection
+const menuBtn = document.querySelector(".site__header--burger");
+const menu = document.querySelector(".site__header--menu");
+const menuBranding = document.querySelector(".menu-branding");
+const menuNav = document.querySelector(".menu-nav");
+const menuNavItem = document.querySelectorAll(".nav-item");
