1205890b0607bd74cda011b7760f824f7fbcfeaf
kgaolodevs <kgaolodev@gmail.com>
2020-07-27 19:54:39 +0200

    Started javascript for burger menu

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,4 +3,4 @@ const menuBtn = document.querySelector(".site__header--burger");
 const menu = document.querySelector(".site__header--menu");
 const menuBranding = document.querySelector(".menu-branding");
 const menuNav = document.querySelector(".menu-nav");
-const menuNavItem = document.querySelectorAll(".nav-item");
+const navItems = document.querySelectorAll(".nav-item");
